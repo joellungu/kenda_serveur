e6cf84f4212c577334afffc08662cd19ece637ac
joellungu <lungujoel138@gmail.com>
2025-02-18 15:33:01 +0100

    Amelioration du mode de paiement.

--- src/main/java/org/kenda/controllers/PaiementController.java
+++ src/main/java/org/kenda/controllers/PaiementController.java
@@ -17,38 +17,62 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.HashMap;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+
 
 @Path("/paiement")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class PaiementController {
 
+    private final Map<String, CompletableFuture<String>> waitingRequests = new ConcurrentHashMap<>();
 
     public String lancer(String devise, String telephone, Double m, String reference) {
-        System.out.println("la devise: $"+devise+" lE MONTANT: $"+m);
-        String dev = devise.equals("USD") ? "USD":"CDF";
-        double montant = conversion(m,1L, devise.equals("USD"));
-        System.out.println("la devise: $"+dev+" lE MONTANT: $"+montant);
-        String urlPost = "https://beta-backend.flexpay.cd/api/rest/v1/paymentService";
-        //////////////////http://41.243.7.46:3006/api/rest/v1/paymentService
-        //flexpay
+        System.out.println("la devise: $" + devise + "lE MONTANT: $" + m);
+        // String dev = devise == "USD" ? "USD":"CDF";
+        // double montant = deviseMetier.conversion(m,1L, devise=="USD");
+        //
+        String dev = devise.equals("USD") ? "USD" : "CDF";
+        double montant = conversion(m, 1L, devise.equals("USD"));
+        //
+        System.out.println("la devise: $" + devise + "lE MONTANT: $" + montant);
+        // String urlPost =
+        // "http://41.243.7.46:3006/flexpay/api/rest/v1/paymentService";
+        ////////////////// http://41.243.7.46:3006/api/rest/v1/paymentService
+
+        String urlPost = "https://backend.flexpay.cd/api/rest/v1/paymentService";
         String body = "{\n" +
-                "  \"merchant\":\"KACHIDI_BINARY\"," +
+                "  \"merchant\":\"Min_EDU-NC\"," +
                 "  \"type\":1," +
-                "  \"reference\": \""+reference+"\"," +
-                "  \"phone\": \""+telephone+"\"," +
-                "  \"amount\": \""+montant+"\"," +
-                "  \"currency\":\""+devise+"\"," +
-                "  \"callbackUrl\":\"http://dgc-epst.uc.r.appspot.com\"" +
+                "  \"reference\": \"" + reference + "\"," +
+                "  \"phone\": \"" + telephone + "\"," +
+                "  \"amount\": \"" + montant + "\"," +
+                "  \"currency\":\"" + dev + "\"," +
+                "  \"callbackUrl\":\"https://epst-serveur-a595d15d6608.herokuapp.com/paiement/trigger\"" +
                 "}";
+        /*
+         * //montant
+         * String body = "{\n" +
+         * "  \"merchant\":\"EPSTAPP\"," +
+         * "  \"type\":1," +
+         * "  \"reference\": \""+reference+"\"," +
+         * "  \"phone\": \""+telephone+"\"," +
+         * "  \"amount\": \""+montant+"\"," +
+         * "  \"currency\":\""+dev+"\"," +
+         * "  \"callbackUrl\":\"http://dgc-epst.uc.r.appspot.com\"" +
+         * "}";
+         */
         var requete = HttpRequest.newBuilder()
                 .uri(URI.create(urlPost))
-                .header("Content-Type","application/json")
-                .header("Authorization","Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJcL2xvZ2luIiwicm9sZXMiOlsiTUVSQ0hBTlQiXSwiZXhwIjoxNzc3NTQzNDc3LCJzdWIiOiI1YzFhMWM5NjQwMGFkODBkMGVlMmU5OWY0NDlhYjYwZiJ9.fVwocevB2T-ag46QGxiCqEvBC3zyPCqpgL4vONIlj2w")
+                .header("Content-Type", "application/json")
+                .header("Authorization",
+                        "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJcL2xvZ2luIiwicm9sZXMiOlsiTUVSQ0hBTlQiXSwiZXhwIjoxNzk0NzYxNDYzLCJzdWIiOiJlZGZiYTY0ZTYxNjM1NWMzYjdjZDJjYzZiZTA5NzMzYiJ9.1gps60CJKzY1CP8XgAEvx8ArRAfqD9v5a9PeJr4qA6c")
+                // .header("Authorization","eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0MkkydjNXQkhUUVdpTlg4ejhQVSIsInJvbGVzIjpbIk1FUkNIQU5UIl0sImlzcyI6Ii9sb2dpbiIsImV4cCI6MTczNTY4NjAwMH0.b3H5IvM1cNtQ5I3Xz3Rf3hBO_pbgFgQ5VpdKrFUI3g0")
+                // .header("Authorization","Bearer
+                // eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJcL2xvZ2luIiwicm9sZXMiOlsiTUVSQ0hBTlQiXSwiZXhwIjoxNzM3NTUyMDEwLCJzdWIiOiI4ZTE4NzJlODQwZTc5YjM5OWIxMDliMmYyNjk5YWY3YSJ9.co6sS0YEdCy3v3nja0NHvS5dYnMNmjZPJET_Ri7pB0E")
                 .POST(HttpRequest.BodyPublishers.ofString(body))
                 .build();
         var client = HttpClient.newHttpClient();
@@ -65,31 +89,32 @@ public class PaiementController {
             return "";
         }
 
-        //return "";
+        // return "";
     }
 
     public String checklancer(String orderNumer) {
 
-        String urlPost = "https://beta-backend.flexpay.cd/api/rest/v1/check/"+orderNumer;
-        //////////////////http://41.243.7.46:3006/api/rest/v1/paymentService
+        String urlPost = "https://beta-backend.flexpay.cd/api/rest/v1/check/" + orderNumer;
+        ////////////////// http://41.243.7.46:3006/api/rest/v1/paymentService
         /*
-        // flexpay
-        String body = "{\n" +
-                "  \"merchant\":\"KACHIDI_BINARY\"," +
-                "  \"type\":1," +
-                "  \"reference\": \""+reference+"\"," +
-                "  \"phone\": \""+telephone+"\"," +
-                "  \"amount\": \""+m+"\"," +
-                "  \"currency\":\""+devise+"\"," +
-                "  \"callbackUrl\":\"http://dgc-epst.uc.r.appspot.com\"" +
-                "}";
-        */
+         * // flexpay
+         * String body = "{\n" +
+         * "  \"merchant\":\"KACHIDI_BINARY\"," +
+         * "  \"type\":1," +
+         * "  \"reference\": \""+reference+"\"," +
+         * "  \"phone\": \""+telephone+"\"," +
+         * "  \"amount\": \""+m+"\"," +
+         * "  \"currency\":\""+devise+"\"," +
+         * "  \"callbackUrl\":\"http://dgc-epst.uc.r.appspot.com\"" +
+         * "}";
+         */
         var requete = HttpRequest.newBuilder()
                 .uri(URI.create(urlPost))
-                .header("Content-Type","application/json")
-                .header("Authorization","Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJcL2xvZ2luIiwicm9sZXMiOlsiTUVSQ0hBTlQiXSwiZXhwIjoxNzc3NTQzNDc3LCJzdWIiOiI1YzFhMWM5NjQwMGFkODBkMGVlMmU5OWY0NDlhYjYwZiJ9.fVwocevB2T-ag46QGxiCqEvBC3zyPCqpgL4vONIlj2w")
+                .header("Content-Type", "application/json")
+                .header("Authorization",
+                        "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJcL2xvZ2luIiwicm9sZXMiOlsiTUVSQ0hBTlQiXSwiZXhwIjoxNzc3NTQzNDc3LCJzdWIiOiI1YzFhMWM5NjQwMGFkODBkMGVlMmU5OWY0NDlhYjYwZiJ9.fVwocevB2T-ag46QGxiCqEvBC3zyPCqpgL4vONIlj2w")
                 .GET()
-                //.POST(HttpRequest.BodyPublishers.ofString(body))
+                // .POST(HttpRequest.BodyPublishers.ofString(body))
                 .build();
         var client = HttpClient.newHttpClient();
         try {
@@ -105,51 +130,51 @@ public class PaiementController {
             return "";
         }
 
-        //return "";
+        // return "";
     }
-
-
 
     Toolkit toolkit;
     Timer timer;
 
     public void AnnoyingBeep() {
-        String reponse = lancer("","",1.0,"");
+        String reponse = lancer("", "", 1.0, "");
         System.out.println(reponse);
-        //reponse = "{"+reponse+"}";
-        System.out.println("{"+reponse+"}");
+        // reponse = "{"+reponse+"}";
+        System.out.println("{" + reponse + "}");
         System.out.println(reponse.getClass());
-        //ObjectMapper obj = new ObjectMapper();
+        // ObjectMapper obj = new ObjectMapper();
         //
         try {
             /*
-            JSONObject obj = new JSONObject(reponse);
-            //JsonNode jn = obj.readTree(reponse);
-            String c1 = obj.get("code").toString();
-            String c2 = obj.get("message").toString();
-            String c3 = obj.get("orderNumber").toString();
-
-            System.out.println(obj.get("code").toString());
-            System.out.println(obj.get("message").toString());
-            System.out.println(obj.get("orderNumber").toString());
-            System.out.println("____________________________________________");
-            //
-            if(c1.equals("0")) {
-                toolkit = Toolkit.getDefaultToolkit();
-                timer = new Timer();
-                timer.schedule(new RemindTask(),
-                        5 * 1000,        //initial delay
-                        5 * 1000);  //subsequent rate
-            }
-            */
+             * JSONObject obj = new JSONObject(reponse);
+             * //JsonNode jn = obj.readTree(reponse);
+             * String c1 = obj.get("code").toString();
+             * String c2 = obj.get("message").toString();
+             * String c3 = obj.get("orderNumber").toString();
+             * 
+             * System.out.println(obj.get("code").toString());
+             * System.out.println(obj.get("message").toString());
+             * System.out.println(obj.get("orderNumber").toString());
+             * System.out.println("____________________________________________");
+             * //
+             * if(c1.equals("0")) {
+             * toolkit = Toolkit.getDefaultToolkit();
+             * timer = new Timer();
+             * timer.schedule(new RemindTask(),
+             * 5 * 1000, //initial delay
+             * 5 * 1000); //subsequent rate
+             * }
+             */
             //
         } catch (Exception e) {
             System.out.println(e);
         }
 
     }
+
     class RemindTask extends TimerTask {
         int numWarningBeeps = 5;
+
         public void run() {
             if (numWarningBeeps > 0) {
                 toolkit.beep();
@@ -160,7 +185,7 @@ public class PaiementController {
                 System.out.println("Time's up!");
                 timer.cancel(); // Not necessary because
                 // we call System.exit
-                //System.exit(0);   // Stops the AWT thread
+                // System.exit(0); // Stops the AWT thread
                 // (and everything else)
             }
         }
@@ -172,71 +197,45 @@ public class PaiementController {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response lancerPaiment(CommandeService commandeService
-                                  //HashMap paiement
+    // HashMap paiement
     ) throws InterruptedException, JsonProcessingException {
         //
-
-        //System.out.println("Le montant: "+paiement.amount);
-        //System.out.println("Le devise: "+paiement.callbackurl);
-        //System.out.println("Le phone: "+paiement.phone);
-        //System.out.println("Le montant: ");
-        String reponse = "";
-        Response repData = null;
-        //paiement.persist();
-        //AnnoyingBeep();
         //
-        String rep = lancer(commandeService.currency, commandeService.phone, commandeService.amount, commandeService.reference);
+        String rep = lancer(commandeService.currency, commandeService.phone, commandeService.amount,
+                commandeService.reference);
+        CompletableFuture<String> future = new CompletableFuture<>();
+        waitingRequests.put(commandeService.reference, future);
         ObjectMapper obj = new ObjectMapper();
-        JsonNode jsonNode = obj.readTree(rep);
-        for(int x = 4; x < 5; x++){
+
+        try {
+            // Attendre que la future soit complétée par une autre requête
+            String result = future.get(); // Bloque jusqu'à ce que la future soit complétée
+
+            JsonNode repCheck = obj.readTree(result);
+            //repCheck["transaction"]['status']
+            if(repCheck.get("code").asText().equals("0") ||
+                    repCheck.get("transaction").get("status").asInt() == (0)){
+                String reponse = "Paiement éffectué";
+                commandeService.ticketList.forEach((t)-> t.persist());
+                return Response.status(200).entity(reponse).build();
+
+            } else {
+                //
+                String reponse = "Paiement non éffectué";
+                //
+                return Response.status(404).entity(reponse).build();
+            }
             //
 
-            //jsonNode
-            //rep['orderNumber']
-            JsonNode repCheck = obj.readTree(checklancer(jsonNode.get("orderNumber").asText()));
-            //repCheck["transaction"]['status']
-            if(repCheck.get("transaction").get("status").asText().equals("0") ||
-                    repCheck.get("transaction").get("status").asInt() == (0)){
-                reponse = "Paiement éffectué";
-                commandeService.ticketList.forEach((t)-> t.persist());
-                repData = Response.status(200).entity(reponse).build();
-                break;
-            }
-
-            if(repCheck.get("transaction").get("status").asText().equals("1") ||
-                    repCheck.get("transaction").get("status").asInt() == (1)){
-                reponse = repCheck.get("message").asText();
-                repData = Response.status(404).build();
-                break;
-            }
-
-            if(repCheck.get("transaction").get("status").asText().equals("3") ||
-                    repCheck.get("transaction").get("status").asInt() == (3)){
-                reponse = repCheck.get("message").asText();
-                repData = Response.status(404).build();
-                break;
-            }
-
-            if(repCheck.get("transaction").get("status").asText().equals("4") ||
-                    repCheck.get("transaction").get("status").asInt() == (4)){
-                reponse = repCheck.get("message").asText();
-                repData = Response.status(404).build();
-                break;
-            }
-
-            if(repCheck.get("transaction").get("status").asText().equals("5") ||
-                    repCheck.get("transaction").get("status").asInt() == (5)){
-                reponse = repCheck.get("message").asText();
-                repData = Response.status(404).build();
-                break;
-            }
-
-            System.out.println("La vérification: "+repCheck.asText());
-
-            TimeUnit.SECONDS.sleep(30);
-
+            //return Response.ok(result).build();
+        } catch (ExecutionException e) {
+            // return Response.serverError().entity("Error: " + e.getMessage()).build();
+            return Response.status(404).entity(e.getMessage()).build();
+        } finally {
+            // Nettoyer la future après utilisation
+            waitingRequests.remove("joellungu123");
+            // return Response.serverError().entity("Error: " + e.getMessage()).build();
         }
-        return repData;
     }
 
     @Path("check/{orderNumer}")
@@ -246,13 +245,13 @@ public class PaiementController {
     public String checkPaiment(@PathParam("orderNumer") String orderNumer) {
         //
 
-        //System.out.println("Le montant: "+paiement.amount);
-        //System.out.println("Le devise: "+paiement.callbackurl);
-        //System.out.println("Le phone: "+paiement.phone);
-        //System.out.println("Le montant: ");
+        // System.out.println("Le montant: "+paiement.amount);
+        // System.out.println("Le devise: "+paiement.callbackurl);
+        // System.out.println("Le phone: "+paiement.phone);
+        // System.out.println("Le montant: ");
 
-        //paiement.persist();
-        //AnnoyingBeep();
+        // paiement.persist();
+        // AnnoyingBeep();
         //
         return checklancer(orderNumer);
     }
@@ -289,50 +288,104 @@ public class PaiementController {
         try {
             Devise devise = Devise.findAll().firstResult();
             return devise.taux;
-        }catch (Exception ex){
+        } catch (Exception ex) {
             return 0;
         }
     }
+
     @Path("/paiee")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public String verificationPaiment() {
         //
-        //AnnoyingBeep();
+        // AnnoyingBeep();
         //
-        //return lancer("",1,"");
+        // return lancer("",1,"");
         return "";
     }
 
     /*
-    @Path("/devise")
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    @Transactional
-    public void setDevise(Devise devise) {
-        //
-        devise.persist();
-        //
-    }
-    */
+     * @Path("/devise")
+     * 
+     * @POST
+     * 
+     * @Consumes(MediaType.APPLICATION_JSON)
+     * 
+     * @Produces(MediaType.APPLICATION_JSON)
+     * 
+     * @Transactional
+     * public void setDevise(Devise devise) {
+     * //
+     * devise.persist();
+     * //
+     * }
+     */
 
     //
     private double conversion(Double montant, Long id, Boolean de) {
         Devise devise = Devise.findAll().firstResult();
-        if(devise != null){
-            System.out.println("Taux: "+devise.taux);
+        if (devise != null) {
+            System.out.println("Taux: " + devise.taux);
         }
         double d = devise != null ? devise.taux : 2027;
         double prct = (7 * montant) / 100;
         if (de) {
-            System.out.println("En dollar: "+de);
+            System.out.println("En dollar: " + de);
             return (montant + prct) / d;
         } else {
-            System.out.println("En franc: "+de);
+            System.out.println("En franc: " + de);
             return montant + prct;
         }
+    }
+
+    @POST
+    @Path("/trigger")
+    public Response triggerRequest(String reponse) {
+        /*
+         * if (waitingRequests.isEmpty()) {
+         * return Response.ok("No requests are waiting.").build();
+         * }
+         */
+        //
+        System.out.println("Reponse: " + reponse);
+        //
+        HashMap hashRep = new HashMap<>();
+        //
+        ObjectMapper mapper = new ObjectMapper();
+        //
+        try {
+            JsonNode result = mapper.readTree(reponse);
+            //
+            // Compléter la première future en attente (ou une spécifique selon votre
+            // logique)
+            Iterator iterator = waitingRequests.entrySet().iterator();
+            //
+            while (iterator.hasNext()) {
+                //
+                Map.Entry<String, CompletableFuture<String>> entry = (Map.Entry<String, CompletableFuture<String>>) iterator
+                        .next();
+                //
+                String requestId = entry.getKey();
+                System.out.println("requestId: " + requestId);
+                if (result.get("reference").asText().equals(requestId)) {
+                    //
+                    CompletableFuture<String> future = entry.getValue();
+                    //
+                    // Compléter la future pour débloquer la requête en attente
+                    //
+                    future.complete(reponse);
+                }
+                //
+            }
+            //
+        } catch (Exception ex) {
+            System.out.println("Erreur 1: " + ex.getMessage());
+            System.out.println("Erreur 2: " + ex.getCause());
+            // future.complete(reponse);
+        }
+
+        return Response.ok("Ok").build();
     }
 
 }
